44953020cd1576dfa63ef79a7e285460cc2e84f5
chenjie <chenjie@szy.cn>
2023-08-31 17:57:22 +0800

    chore: 注释优化

--- src/router/modules/demo.ts
+++ src/router/modules/demo.ts
@@ -5,14 +5,14 @@ export default {
   path: '/demo',
   component: RouterView,
   children: [
+    // 示例首页
     {
-      // 示例首页
       path: 'index',
       meta: { title: '首页' },
       component: () => import('@/pages/demo/index.vue')
     },
+    // 示例结果页
     {
-      // 示例结果页
       path: 'result',
       meta: { title: '结果页' },
       component: () => import('@/pages/demo/result.vue')
