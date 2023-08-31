f834f2365726406e16d26a21d5b37b56cc95d17d
chenjie <chenjie@szy.cn>
2023-08-31 11:53:31 +0800

    feat: 调整 vite 配置

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,10 @@ import express from 'express'
 
 const APP_ENV = process.env.VITE_APP_ENV as string
 
+// eslint-disable-next-line no-control-regex
+const INVALID_CHAR_REGEX = /[\u0000-\u001F"#$&*+,:;<=>?[\]^`{|}\u007F]/g
+const DRIVE_LETTER_REGEX = /^[a-z]:/i
+
 const HTTPMockerPlugin = (): Plugin => {
   let config: ResolvedConfig | undefined
   return {
@@ -47,7 +51,23 @@ export default defineConfig((config) => {
       }
     },
     build: {
-      target: 'esnext'
+      target: 'esnext',
+      // 主要解决 GitHub 页面阻止了以下划线字符开头的文件 https://github.com/vitejs/vite/issues/9119
+      rollupOptions: {
+        output: {
+          // https://github.com/rollup/rollup/blob/master/src/utils/sanitizeFileName.ts
+          sanitizeFileName(name) {
+            const match = DRIVE_LETTER_REGEX.exec(name)
+            const driveLetter = match ? match[0] : ''
+            // A `:` is only allowed as part of a windows drive letter (ex: C:\foo)
+            // Otherwise, avoid them because they can refer to NTFS alternate data streams.
+            return (
+              driveLetter +
+              name.slice(driveLetter.length).replace(INVALID_CHAR_REGEX, '')
+            )
+          }
+        }
+      }
     },
     plugins: [legacy(), vue(), vueJsx(), HTTPMockerPlugin()]
   }
