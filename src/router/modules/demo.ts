72eb95edfb588b22d31550f9974d8d8117872587
chenjie <chenjie@szy.cn>
2023-08-31 18:01:36 +0800

    feat: 新增路由配置

--- src/router/modules/demo.ts
+++ src/router/modules/demo.ts
@@ -2,17 +2,20 @@ import { type RouteRecordRaw, RouterView } from 'vue-router'
 
 /** 示例相关页面 */
 export default {
+  name: 'Demo',
   path: '/demo',
   component: RouterView,
   children: [
     // 示例首页
     {
+      name: 'DemoIndex',
       path: 'index',
       meta: { title: '首页' },
       component: () => import('@/pages/demo/index.vue')
     },
     // 示例结果页
     {
+      name: 'DemoResult',
       path: 'result',
       meta: { title: '结果页' },
       component: () => import('@/pages/demo/result.vue')
