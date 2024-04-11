e63d577901377f7437c41fed166a25c1d9700ed3
chenjie <chenjie@szy.cn>
2024-04-11 16:38:01 +0800

    feat: 项目依赖优化

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,7 @@ module.exports = {
   },
   rules: {
     'vue/multi-word-component-names': 'off',
-    '@typescript-eslint/no-non-null-assertion': 'off'
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn'
   }
 }
